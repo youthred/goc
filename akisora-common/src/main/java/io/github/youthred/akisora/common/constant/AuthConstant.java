c532bcf1ae18232cfde4233a8ff213a663ba4043
youthred <xxooec@gmail.com>
2022-07-29 21:19:10 +0800

    gateway

---
+++ akisora-common/src/main/java/io/github/youthred/akisora/common/constant/AuthConstant.java
@@ -0,0 +1,7 @@
+package io.github.youthred.akisora.common.constant;
+
+public class AuthConstant {
+
+    public static final String AUTHORITY_PREFIX = "ROLE_";
+    public static final String AUTHORITY_CLAIM_NAME = "authorities";
+}
---
+++ akisora-gateway/src/main/java/io/github/youthred/akisora/gateway/config/properties/IgnoreUrlsProperties.java
@@ -0,0 +1,17 @@
+package io.github.youthred.akisora.gateway.config.properties;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Component
+@ConfigurationProperties(prefix="secure.ignore")
+public class IgnoreUrlsProperties {
+
+    private List<String> urls;
+}
