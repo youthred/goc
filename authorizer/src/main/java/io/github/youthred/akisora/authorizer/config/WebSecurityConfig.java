761f48eec1fc5fcb9af44942e12bf4590800a3a2
youthred <xxooec@gmail.com>
2022-07-29 20:34:29 +0800

    .

--- authorizer/src/main/java/io/github/youthred/akisora/authorizer/config/WebSecurityConfig.java
+++ authorizer/src/main/java/io/github/youthred/akisora/authorizer/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
-                .antMatchers("/rsa/publicKey", "/oauth/token").permitAll()
+                .antMatchers("/rsa/publicKey").permitAll()
                 .anyRequest().authenticated();
     }
 
--- authorizer/src/main/java/io/github/youthred/akisora/authorizer/service/UserServiceImpl.java
+++ authorizer/src/main/java/io/github/youthred/akisora/authorizer/service/UserServiceImpl.java
@@ -1,6 +1,13 @@
 package io.github.youthred.akisora.authorizer.service;
 
+import cn.hutool.core.collection.CollUtil;
+import io.github.youthred.akisora.authorizer.config.SecurityUser;
+import io.github.youthred.akisora.common.constant.MessageConstant;
 import io.github.youthred.akisora.entity.User;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,25 +17,36 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserDetailsService {
 
-    private final PasswordEncoder passwordEncoder;
-
-    private List<User> users;
+    private final List<User> users;
 
     public UserServiceImpl(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
         users = new ArrayList<>(1);
         users.add(
-                new User().setId(123L).setUsername("admin").setPassword(this.passwordEncoder.encode("123456")).setStatus(1).setRoles(Collections.singletonList("ADMIN"))
+                new User().setId(123L).setUsername("admin").setPassword(passwordEncoder.encode("123456")).setStatus(1).setRoles(Collections.singletonList("ADMIN"))
         );
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        passwordEncoder.encode("123");
-        return null;
+        List<User> findUserList = users.stream().filter(item -> item.getUsername().equals(username)).collect(Collectors.toList());
+        if (CollUtil.isEmpty(findUserList)) {
+            throw new UsernameNotFoundException(MessageConstant.USERNAME_PASSWORD_ERROR);
+        }
+        SecurityUser securityUser = new SecurityUser(findUserList.get(0));
+        if (!securityUser.isEnabled()) {
+            throw new DisabledException(MessageConstant.ACCOUNT_DISABLED);
+        } else if (!securityUser.isAccountNonLocked()) {
+            throw new LockedException(MessageConstant.ACCOUNT_LOCKED);
+        } else if (!securityUser.isAccountNonExpired()) {
+            throw new AccountExpiredException(MessageConstant.ACCOUNT_EXPIRED);
+        } else if (!securityUser.isCredentialsNonExpired()) {
+            throw new CredentialsExpiredException(MessageConstant.CREDENTIALS_EXPIRED);
+        }
+        return securityUser;
     }
 }
