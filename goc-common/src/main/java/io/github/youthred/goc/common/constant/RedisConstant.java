affff2fe93ae781ee4486ad9eaac12d61ca4c6eb
youthred <xxooec@gmail.com>
2022-07-30 16:18:58 +0800

    redis save test

---
+++ goc-common/src/main/java/io/github/youthred/goc/common/constant/RedisConstant.java
@@ -0,0 +1,6 @@
+package io.github.youthred.goc.common.constant;
+
+public class RedisConstant {
+
+    public static final String GOC_RESOURCE_ROLES_MAP = "GOC_AUTH:RESOURCE_ROLES_MAP";
+}
---
+++ goc-test-api/src/main/java/io/github/youthred/goc/testapi/rest/RedisController.java
@@ -0,0 +1,30 @@
+package io.github.youthred.goc.testapi.rest;
+
+import cn.hutool.core.collection.CollUtil;
+import io.github.youthred.goc.common.constant.RedisConstant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+@RestController
+@RequestMapping("/r")
+@RequiredArgsConstructor
+public class RedisController {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @GetMapping("/save")
+    public void save() {
+        Map<String, List<String>> resourceRolesMap;
+        resourceRolesMap = new TreeMap<>();
+        resourceRolesMap.put("/test-api/hello", CollUtil.toList("ADMIN"));
+        resourceRolesMap.put("/test-api/user/currentUser", CollUtil.toList("ADMIN", "TEST"));
+        redisTemplate.opsForHash().putAll(RedisConstant.GOC_RESOURCE_ROLES_MAP, resourceRolesMap);
+    }
+}
